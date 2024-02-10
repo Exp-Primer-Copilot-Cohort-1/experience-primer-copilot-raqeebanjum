546c660d4472a920cea28f99c53c1fd2dfd92326
Raqeeb Anjum <118586717+raqeebanjum@users.noreply.github.com>
2024-02-10 19:57:14 +0000

    Copilot third commit

--- member.js
+++ member.js
@@ -1,1 +1,15 @@
-function skillsMember()
+// Exporting a function
+function skillsMember() {
+  // Your code here
+}
+
+// Exporting an object
+const member = {
+  // Your code here
+};
+
+// Exporting multiple functions or objects
+module.exports = {
+  skillsMember,
+  member
+};
